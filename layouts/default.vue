740f3c6cde49fc7053e170ff23257111eb56bda8
LeeJunyong <iiiuooyoo@gmail.com>
2026-06-26 11:59:57 +0900

    chore : GA 제거

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,5 @@
 <template>
     <Head>
-        <!-- Google tag (gtag.js) -->
-        <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-QKFLJJ18PF"
-        ></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag() {
-                dataLayer.push(arguments);
-            }
-            gtag("js", new Date());
-
-            gtag("config", "G-QKFLJJ18PF");
-        </script>
         <Title>생일선물 뽑기</Title>
         <Meta name="theme-color" content="rgb(26 136 233)" />
         <Meta name="description" content="이준용이 주는 생일선물!" />
